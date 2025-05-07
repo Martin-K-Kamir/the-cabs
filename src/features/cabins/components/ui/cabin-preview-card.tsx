8e7a076bc9e6026400ed594f1b18b6deb2835825
Martin Kamír <martin.k.kamir@gmail.com>
2025-05-07 21:19:11 +0200

    test

--- src/features/cabins/components/ui/cabin-preview-card.tsx
+++ src/features/cabins/components/ui/cabin-preview-card.tsx
@@ -1,12 +1,18 @@
 // import Image from "next/image";
 import Link from "next/link";
 import { Skeleton } from "@/components/ui/skeleton";
-// import { CarouselDots, CarouselItem } from "@/components/ui/carousel";
-// import { ImagesGalleryCarousel } from "@/components/ui/images-gallery";
 import { ReviewsSummary } from "@/features/reviews/components/ui/reviews-summary";
 import { BookingPrice } from "@/features/bookings/components/ui/booking-price";
 import type { CabinId } from "@/features/cabins/lib/types";
 import { cn, formatCompactDateRange } from "@/lib/utils";
+import {
+    Carousel,
+    CarouselContent,
+    CarouselDots,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from "@/components/ui/carousel";
 
 export type CabinPreviewCardProps = {
     id: CabinId;
@@ -30,6 +36,7 @@ export function CabinPreviewCard({
     id,
     name,
     price,
+    images,
     discount,
     className,
     ratings,
@@ -86,6 +93,48 @@ export function CabinPreviewCard({
             >
                 <CarouselDots tabIndex={-1} />
             </ImagesGalleryCarousel> */}
+
+            <Carousel className="relative aspect-[1.1/1] overflow-hidden rounded-xl transition-[scale] group-hover/cabin-preview:scale-[103%]">
+                <CarouselContent className="relative m-0">
+                    {images.map(image => (
+                        <CarouselItem
+                            key={image}
+                            className="relative aspect-[1.1/1] overflow-hidden p-0 first:rounded-l-xl last:rounded-r-xl"
+                        >
+                            <CarouselItem
+                                key={image}
+                                className="relative aspect-[1.1/1] overflow-hidden p-0 first:rounded-l-xl last:rounded-r-xl"
+                            >
+                                {/* <Link
+                                    href={`/cabins/${id}`}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="absolute inset-0 rounded-xl outline-none"
+                                >
+                                    <Image
+                                        fill
+                                        src={image}
+                                        alt={`Image ${index + 1} of ${images.length} showcasing the ${name}`}
+                                        className="object-cover"
+                                        priority={true}
+                                        sizes="(max-width: 600px) 100vw, 600px"
+                                    />
+                                </Link> */}
+                                hi
+                            </CarouselItem>
+                        </CarouselItem>
+                    ))}
+                </CarouselContent>
+                <CarouselPrevious
+                    hideOnDisabled
+                    className="opacity-0 transition-[opacity,scale] hover:scale-105 focus:scale-105 group-hover/cabin-preview:opacity-100 group-has-[*:focus-visible]/cabin-preview:opacity-100 lg:left-4 lg:size-8"
+                />
+                <CarouselNext
+                    hideOnDisabled
+                    className="opacity-0 transition-[opacity,scale] hover:scale-105 focus:scale-105 group-hover/cabin-preview:opacity-100 group-has-[*:focus-visible]/cabin-preview:opacity-100 lg:left-4 lg:size-8"
+                />
+                <CarouselDots tabIndex={-1} />
+            </Carousel>
 
             <section className="mt-2">
                 <header className="flex items-center justify-between">
